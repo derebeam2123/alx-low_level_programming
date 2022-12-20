a2271f4ebec405015912429cc287258f56b64db6
nahom <derebeam2123@gmail.com>
2022-12-20 07:06:43 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,17 @@
  * main - Entry point
  * Return: Always 0
  */
+int main(void)
 {
-	_putchar(' ');
-	_putchar('p';
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
 
