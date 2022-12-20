0f780d7cb774e4e0ee58a9ee2ba09ba1d9d0ce45
nahom <derebeam2123@gmail.com>
2022-12-19 19:19:54 -0800

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if  (n > 0)
 	{
 		printf("% is positive\n", n);
 	}
@@ -27,5 +27,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
-
 }
