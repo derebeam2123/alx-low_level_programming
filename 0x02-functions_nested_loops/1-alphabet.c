9c80f9255e2e9f84bc9594c8a8b71eda09517d26
nahom <derebeam2123@gmail.com>
2022-12-20 15:16:11 -0800

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,15 @@
  * Return: 0
  */
 
-void print_alphabet(void)
+void print_alphabet(void c) 
 {
 	char c  = 'a';
 
 	while (c <= 'z');
 	{
-		_putchar(c);
-		c++;
+
+	_putchar(c);
+	c++;
 	}
 	_putchar('\n');
 }
