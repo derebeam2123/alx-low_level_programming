be662ccdf8b9db539e91904e9f1740b72bff93bf
nahom <derebeam2123@gmail.com>
2022-12-20 03:45:32 -0800

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,9 +2,10 @@
 
 /**
  * swap_int - function that swaps the values of two integers. 
- * @a: this is the first entry
- * @b: this is the second entry 
  *
+ * @a: this is the first entry
+ * 
+ * @b: this is the second entry 
  *
  */
 
