9e6075a7ef8c858dbb783af0c042adf1d16beaf4
nahom <derebeam2123@gmail.com>
2022-12-20 02:21:07 -0800

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  * reset_to_98 -function that takes a pointer to an int as parameter and
  * update the valur it to 98.
  *
- * on : this is the empty
+ * @n: this is the empty
  */
 void reset_to_98(int *n)
 {
