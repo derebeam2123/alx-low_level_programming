aa5df49dfeb2284d726ef1ab15bf5b53b31173b6
nahom <derebeam2123@gmail.com>
2022-12-19 22:39:03 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include  <stdio.h>
+#include <stdlid.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char low;
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	for (low = 'A'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
+
