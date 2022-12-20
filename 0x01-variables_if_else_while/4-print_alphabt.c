a499a2c7eac0957f5cc6a8bbaa594747cd93da07
nahom <derebeam2123@gmail.com>
2022-12-19 23:13:06 -0800

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print "programing is like building ammulilingual"
+ * puzzle, followde by a newline.
+ * Return 0,
+ */
+int main(void)
+
+{
+	puts("\"programing is like building ammulilingual");
+
+	return (0);
+}
