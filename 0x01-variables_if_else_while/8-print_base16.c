5f57920d57b9a729457b24d380ce8b939cc64238
nahom <derebeam2123@gmail.com>
2022-12-20 01:16:03 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <dtdio.h>
+#include  <stdlid.h>
+#include <time.h>
+
+/**
+ * main - Entry  point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d); 
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low); 
+	putchar('\n');
+	
+	return (0);
+}       
