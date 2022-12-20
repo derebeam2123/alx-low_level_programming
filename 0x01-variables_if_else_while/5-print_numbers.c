92f41c73112f70a25f79c98a517727e46864a04b
nahom <derebeam2123@gmail.com>
2022-12-19 23:42:43 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>                                                      
+
+                                                                        
+
+/**                                                                     
+ *
+ *  main - print "with proper grammer, but the outcome is a place of art," follwed newline   
+ *  Return: 0;
+ */          
+
+int main(void)                                                          
+
+{                                                                       
+	        puts("with proper grammer, but the outcome is a place of art,\n"); 
+		return (0);
+}
