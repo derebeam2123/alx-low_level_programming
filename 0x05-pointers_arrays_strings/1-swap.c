cdc35461f07056f1867f9370b994d862ce253841
nahom <derebeam2123@gmail.com>
2022-12-20 03:29:48 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,17 @@
 #include "main.h"
-
 /**
- * swap_int - function that swaps the value of two integers.
- * @a: this is the first entry
- * @b: this is the second entry
+ * swap_int - swap the value of two integers using two input parametrs
  *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
  */
+
 void snap_int(int *a, int *b)
 {
 	int changed;
-
+	
 	changed = *a;
 	*a = *b;
 	*b = changed;
