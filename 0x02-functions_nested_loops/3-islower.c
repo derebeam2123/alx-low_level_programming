a8eac8a3e55a8089d22f8acdea13448043d2ea67
nahom <derebeam2123@gmail.com>
2022-12-20 15:59:02 -0800

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * -islower.c - short descriptio. single line
+ *  @c: contain value to be campared
+ *  Return: Always 0.
+ */
+int _islower.c(int c)
+
+{
+	if (c > 'a' && c 'z')
+	{
+		return (1);
+	}
+	
+	else
+	{
+		return (0);
+	}
