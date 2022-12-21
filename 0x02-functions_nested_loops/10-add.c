22b080db2e771a279301ee559019615dcfd60667
nahom <derebeam2123@gmail.com>
2022-12-20 17:42:01 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include "stdio.h"
+/** add - prints add two number
+ * @i: print int i
+ * @k: print int k
+ * Return: Always 0.
+ */
+int add( int i, int k)
+{
+	return (i +k);
+}
