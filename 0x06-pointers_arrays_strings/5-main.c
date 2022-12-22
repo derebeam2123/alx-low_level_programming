1e42bcc0113f5b60dedd495bf990c43e49504310
nahom <derebeam2123@gmail.com>
2022-12-22 10:15:59 -0800

    task5

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -1,23 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * string_toupper - function that changes alllowercase letters
- * of a string to uppercase.
- * @ch: pointer to char
- * return: char
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-char *string_toupper(char *)
+int main(void)
 {
-	int i = 0;
-	
-	while (*(ch + i) != '\0');
-	{
-		if (*(ch + i) >= 97 && *(ch + i) <= 122)
-			*(ch + i) = *(ch + i) - ' ';
-		i++;
-	}
-	return (ch);
+	char str[] = "Look up!\n";
+	char *ptr;
 
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
 
-
-vxvzvxzv
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * string_toupper - function that changes alllowercase letters of a string to uppercas,
+ * string_toupper - function that changes all lowercase letters
+ * of a string to uppercas,
  *
  * @ch: pointer to char
- * return: char
+ * Return: char
  */
 
 char *string_toupper(char *ch)
 {
 	int i = 0;
-	
+
 	while (*(ch + i) != '\0')
+
+
 	{
 		if (*(ch + i) >= 97 && *(ch + i) <= 122)
 		*(ch + i) -= 32;
+		i++;
 	}
-	return (*);
+	return (ch);
+
 }
