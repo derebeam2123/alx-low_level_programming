5b3997f14c5eb0a4a09052e54356c9531b3d001b
nahom <derebeam2123@gmail.com>
2022-12-28 11:51:40 -0800

    task4

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,19 +16,19 @@ void print_remaing_days(int month, int day, int year)
 		{
 			day++;
 		}
-		printf("Day of the year: %d\n". day);
-		printf("Remaining day: %d\n". 366 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining day: %d\n", 366 - day);
 	}
 	else
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n". month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day of the year: %d\n". day);
-			printf("Remaining day: %d\n". 365 - day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining day: %d\n", 365 - day);
 		}
 	}
 }
