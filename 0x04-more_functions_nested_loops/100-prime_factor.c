77354717e3b07adde41e5fa345094d679420beff
nahom <derebeam2123@gmail.com>
2022-12-28 20:01:20 -0800

    task12

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - prints the largest prime factor of the number 612852475143
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long x, maxf;
+	long number = squrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (
