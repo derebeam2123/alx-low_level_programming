8243261fc9b743e090a205efd65b3ca7603e800b
nahom <derebeam2123@gmail.com>
2022-12-31 10:04:54 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints 012356789\n
+ *
+ * Return:void
+ */
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '0'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
