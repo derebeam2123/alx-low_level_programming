4d9bab0e00b0a4f69953b1dcc038f223fa804cc7
nahom <derebeam2123@gmail.com>
2022-12-31 20:29:45 -0800

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * print_triangle - function that prints a triangle
- * @size: size of the tiangle
- * Return: print always "#"
- *
+ * print_triangle - print triangle of squares depend on parametr
+ * @size: the size of the squares tiangle
+ * Return: empty
  */
 void print_triangle(int size)
 {
@@ -14,15 +13,15 @@ _putchar('\n');
 }
 else
 {
-for (a = 0; a <= size; a++)
+for (a = 0; a < size; a++)
 {
-for (b = size - a; b > 1; b--)
+for (b = size = a; b > 1; b--)
 {
-_putchar(' ');
+_putchar(32);
 }
 for (c = 0; c <= a; c++)
 {
-_putchar('#');
+_putchar(35);
 }
 _putchar('\n');
 }
