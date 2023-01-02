8aa25d1122476cf3ff314bf2ebbe9a1d4698dc67
nahom <derebeam2123@gmail.com>
2023-01-02 11:43:35 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,7 +2,7 @@
 /**
  * _strstr - first occurrence of the substring needle in the string haystack
  * @haystack: main str to be examined
- * @needle: search in haystack
+ * @needle: searched in haystack
  * Return: return 0
  */
 char *_strstr(char *haystack, char *needle)
@@ -11,8 +11,8 @@ char *str1, *str2; /*declaring variables*/
 
 while (*haystack != '\0')
 {
-str1 = haystack; /*value*/
-str = needle;
+str1 = haystack; /*values*/
+str2 = needle;
 
 /*star while*/
 while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
