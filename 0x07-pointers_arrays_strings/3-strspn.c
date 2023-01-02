45ec8d3157f5a54375fcb95c0efcf46fb5646f71
nahom <derebeam2123@gmail.com>
2023-01-02 10:37:15 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,10 +10,12 @@ unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j;
 for (j = 0; *(s + j); j++)
+{
 for (j = 0; *(accept + i); i++)
 {
 if (*(s + j) == *(accept + i))
 break;
+}
 if (*(accept + i) == '\0')
 break;
 }
