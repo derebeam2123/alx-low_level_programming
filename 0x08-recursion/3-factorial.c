40e92aeea749603dde7f8b43c0a41c9363bd7a35
nahom <derebeam2123@gmail.com>
2023-01-04 01:58:28 -0800

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - factorial
+ * @n: pointer
+ * Return: factorial
+ */
+int factorial(int n)
+{
+if (n == 0)
+return (1);
+else if (n < 0)
+return (-1);
+else 
+	return (n * factorail(n -1));
+}
+
+
+
