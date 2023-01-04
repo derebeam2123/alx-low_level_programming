732a04d0f83f08e13e27fefb6d99703144ffadbe
nahom <derebeam2123@gmail.com>
2023-01-04 03:19:21 -0800

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * find_sqrt - find the natural number
+ * @num: number
+ * @root: root
+ * Return: find sqrt
+ */
+int find_sqrt(int num, int root)
+{
+if ((root * root) == num)
+	return (root);
+}
