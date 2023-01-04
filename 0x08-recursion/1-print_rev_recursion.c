b34fba65900b81092a2f35abf181421e413c75d6
nahom <derebeam2123@gmail.com>
2023-01-04 00:15:05 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _print_rev_recursion - print dtring reverse.
+ * _print_rev_recursion - print dtringin reverse.
  * @s: string
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
