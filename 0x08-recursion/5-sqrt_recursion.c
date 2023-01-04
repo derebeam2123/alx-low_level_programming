3a937d8d8b552565d4372150c59dfcd4cfe9e5c9
nahom <derebeam2123@gmail.com>
2023-01-04 03:28:15 -0800

    task -5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,11 @@
 #include "main.h"
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 /**
- * find_sqrt - find the natural number
- * @num: number
- * @root: root
+ * find_sqrt - find the natural square root of an insulted number.
+ * @num: The number to find the square root of.
+ * @root: the root to be tested.
+ *
  * Return: find sqrt
  */
 int find_sqrt(int num, int root)
