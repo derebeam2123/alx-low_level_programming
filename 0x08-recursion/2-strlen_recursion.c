c83fa96031e044289724dd1da9ca74a242529035
nahom <derebeam2123@gmail.com>
2023-01-04 00:26:18 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return the length of string
+ * @s: the string
+ * Return: the length
+ */
+int _strlen_recursion(char *s);
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len int _strlen_recursion(s + 1);
+	}
+}
