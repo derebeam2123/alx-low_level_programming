e5b160722efa8a984b2d5c82042867a062eff4ff
nahom <derebeam2123@gmail.com>
2023-01-06 09:30:19 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - write program to print name in lew line.
+ * remove the path before the name of the program
+ * @arc: thid is the argument count
+ * @argv: This is the argument vector
+ * Returen: This return to 0
+ */
+int main(int argc, char *argv[])
+{
+	print("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
