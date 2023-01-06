b77b8be0bdb1272cedd69e3548f30563c7ccb1c7
nahom <derebeam2123@gmail.com>
2023-01-06 09:50:52 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - write program to print name in lew line.
- * remove the path before the name of the program
- * @arc: this is the argument count
+ * main - write a program that print name,followed by a lew line.
+ * If ou rename the program,print new name, without having to compile it again
+ * you should note remove the path before the name of the program
+ * @argc: this is the argument count
  * @argv: This is the argument vector
- * Returen: This return to 0
+ * Return: This return to 0
  */
 int main(int argc, char *argv[])
 {
