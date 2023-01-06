0a5b6ae29fc0a5ded469156510e0965d77329064
nahom <derebeam2123@gmail.com>
2023-01-06 10:32:59 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplication;
+ * @argc: number of arguments
+ * @argv: array of vector
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, multiplication;
+
+	multiplication = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[i]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
