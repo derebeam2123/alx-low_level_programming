f1144b7bd54ed8b9f6701a2230272b968858ac4b
nahom <derebeam2123@gmail.com>
2023-01-06 19:55:21 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,11 +16,41 @@ int check_num(char *str)
 
 	while (count < strlen(str))
 	{
-		if (!isdigit(str))
+		if (!isdigit(str[count]))
 		{
 			return (0);
 		}
 		count++;
 	}
 	return (1);
+}
+/**
+ * main - print name
+ * @argc: Count argument
+ * @argv: argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			print("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
