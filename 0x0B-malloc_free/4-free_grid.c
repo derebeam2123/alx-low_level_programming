940b1729115247dc86ddbb8b7e75fd03f57eb41b
nahom <derebeam2123@gmail.com>
2023-01-09 05:56:05 -0800

    task 1

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "stdio.h"
 /**
- * free_grid - allocates a grid
+ * free_grid - frees the ,e,ory allocate for the grid
+ * created by alloc_grid()
  * @grid: takes in width of grid
  * @height: heigt of the grid
  */
