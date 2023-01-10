6f69f1a69018d70ec523060438ef483d3e957c15
nahom <derebeam2123@gmail.com>
2023-01-10 02:13:45 -0800

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,12 +11,20 @@ char *argstostr(int ac, char **av)
 	int i, j, k, len;
 	char *str;
 
+	len = 0;
+	k = 0;
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
+		for (j = 0; av[i][j] != '\0')
+		{
+		j++;
+		len++;
+
+		}
+		len++;
+		i++;
 	}
 	str = malloc(sizeof(char) * (len + 1));
 
@@ -29,9 +37,12 @@ char *argstostr(int ac, char **av)
 		{
 			str[k] = av[i][j];
 			k++;
+			j++;
 		}
 		str[k] = '\n';
 		k++;
+		i++;
 	}
+	str[k] = '\0';
 	return (str);
 }
