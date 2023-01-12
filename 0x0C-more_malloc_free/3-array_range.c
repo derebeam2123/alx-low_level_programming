7852de670263e27664b87154a9cd369a94a40778
nahom <derebeam2123@gmail.com>
2023-01-12 03:18:25 -0800

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,18 +4,25 @@
  * *array_range - creat an array
  * @min: minimum
  * @max: maximum
- * Return: arr
+ * Return: pointer to array of array.
  */
 int *array_range(int min, int max)
 {
-	int *arr, i = 0, t = min;
+	int range, i;
+	int *p;
+
+
+	range = 0;
 
 	if (min > max)
-		return (0);
-	arr = malloc((max - min + 1) * sizeof(int));
-	if (!arr)
-		return (0);
-	while (i <= max - min)
-		arr[I++] + i++;
-	return (arr);
+		return (NULL);
+	range = ((max + 1) - min);
+	p = malloc(range * sizeof(int));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < range; i++)
+	{
+		*(p + i) = min + i;
+	}
+	return (p);
 }
