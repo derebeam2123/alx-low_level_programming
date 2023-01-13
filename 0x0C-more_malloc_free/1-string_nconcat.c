52c178db0a97fb6a6af2d4010d9c884fa12f549f
nahom <derebeam2123@gmail.com>
2023-01-12 17:31:23 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,38 +1,53 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdlib.h>
 /**
- * *string_nconcat - two string
+ * _strlen - calculet return string
+ * @string: string
+ * Return: string length
+ */
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	       ;
+return (i);
+}
+/**
+ * string_nconcat - two string
  * @s1: first string
  * @s2: second string
  * @n: number
- * Return: null
+ * Return: pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, i;
+	char *ptr;
+	int num, len, i, j;
+
+	num = n;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i]; i++)
-		len++;
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
-
+	if (num < 0)
 		return (NULL);
-	len = 0;
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
 
-	for (i = 0; s1[i]; i++)
-		concat[len++] = s1[i];
+	len =  _strlen(s1) + num + 1;
 
+	ptr = malloc(sizeof(*ptr) * len);
+	if (ptr == NULL)
+		return (NULL);
 
-	for (i = 0; s2[i] && i < n; i++)
-		concate[len++] = s2[i];
+	for (i = 0; s1[i] != '\0';
+			ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+	ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
 
-
-	concat[len] = '\0';
-	return (concat);
+	return (ptr);
 }
