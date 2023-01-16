52ab1b374c669e8fa56ff334c982d98f8381c9b0
nahom <derebeam2123@gmail.com>
2023-01-16 03:32:39 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,10 @@
 #define STRTYP
 
 /**
- * struct dog - struct dog
- * @name: name
- * @age: age
- * @owner: owner
+ * struct dog - structure of a dog
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
  * Description: this struct is for dog
  */
 struct dog
@@ -24,7 +24,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
