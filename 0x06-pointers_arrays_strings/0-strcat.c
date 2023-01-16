ad2be779e66f6949b3346b35e1d5a391af6417be
nahom <derebeam2123@gmail.com>
2023-01-16 13:40:12 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * strcat - cocatenates two string
+ * _istrcat - cocatenates two string
  * @dest: a pointer to the string
  * @src: the source
  *
  * Return: a pointer to the destination string @dest,
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
-
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
