4c5e2646ca608a8c875819b2ffccc560f817c9ea
nahom <derebeam2123@gmail.com>
2023-01-18 01:41:33 -0800

    task 0

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,2 +1,51 @@
 #include "3-calc.h"
-
+/**
+ * op_add - integer
+ * @a: integer
+ * @b: integer
+ * Return: a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - integer
+ * @a: integer
+ * @b: intger
+ * Return: a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - integer
+ * @a: integer
+ * @b: integer
+ * Return: a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - integer
+ * @a: integer
+ * @b: integer
+ * Return: a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - integer
+ * @a: integer
+ * @b: integer
+ * Return: a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
