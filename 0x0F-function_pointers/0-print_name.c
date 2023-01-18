bcf9f1297cc29e0322cf2b35034c995ecb7f3091
nahom <derebeam2123@gmail.com>
2023-01-17 22:58:56 -0800

    task 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: name
+ * @f: print function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name || if)
+		return;
+	f(name);
+}
