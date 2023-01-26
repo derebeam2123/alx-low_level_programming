d3b1567a2856859499ced8e4cc5e965e52ecb8ae
nahom <derebeam2123@gmail.com>
2023-01-26 02:36:19 -0800

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,11 +26,12 @@ list_t *add_node(list_t **head, const char *str)
 
 	for (len = 0; str[len];)
 		len++;
+
 	new->str = dup;
 	new->len = len;
 	new->next = *head;
 
 	*head = new;
 
-	return (NULL);
+	return (new);
 }
