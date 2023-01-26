dc494fdf0809a7d4c9b52596e6215a453b8dd006
nahom <derebeam2123@gmail.com>
2023-01-26 01:42:05 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,11 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 
 		else
-			print("[%d] %s\n", h->len, h->str);
+			print("[%u] %s\n", h->len, h->str);
 		nodes++;
 		h = h->next;
 	}
