4cb7fac852f208cbe2899afffe338f54c18e8ba0
nahom <derebeam2123@gmail.com>
2023-01-30 18:49:36 -0800

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the head
+ * @head: a pointer
+ * Return: empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int ret;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (ret);
+}
