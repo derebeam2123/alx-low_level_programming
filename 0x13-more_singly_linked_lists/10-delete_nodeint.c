1f5d72f40a1f3476a6862c59811ded15326f36f8
nahom <derebeam2123@gmail.com>
2023-01-30 20:41:48 -0800

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (node = 0; node < (index - 1); node++)
 	{
-		if (code->next == NULL)
+		if (copy->next == NULL)
 			return (-1);
 
 		copy = copy->next;
