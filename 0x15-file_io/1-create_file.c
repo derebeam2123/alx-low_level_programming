cd47a782394ad7afc4c2ef5fb5a60e1ecc2c1a82
nahom <derebeam2123@gmail.com>
2023-02-06 19:26:16 -0800

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,24 +7,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text content[len];)
-			len++;
-	}
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
-
-	if (fd == -1 || w == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
+	if (text_content)
+		write(fd, text_contrnt, _strlen(text_content));
+
 
 	close(fd);
 
 	return (1);
+}
+/**
+ * _strlen - strlen
+ * @s: string
+ * Return: length
+ */
+int_strlen(char *s)
+{
+	int c = 0;
+
+	while (S[c])
+		c++;
+	return (c);
 }
