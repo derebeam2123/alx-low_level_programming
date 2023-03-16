7300960b95687e6c1fe4f03d78f008d18c7a91f6
nahom <derebeam2123@gmail.com>
2023-03-16 04:24:30 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
  * print_dlistint - print element
- * @a: the head
+ * @h: the head
  * Return:  number
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
-	while (a)
+	while (h)
 	{
 		nodes++;
-		printf("%d\n", a->n);
-		a = a->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (nodes)
 }
