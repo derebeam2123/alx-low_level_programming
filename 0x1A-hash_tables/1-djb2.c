e4e25e0e07eded346aff9a351eb35174e3d1916c
nahom <derebeam2123@gmail.com>
2023-04-06 23:32:35 -0700

    task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,9 @@
 #include "hash_tables.h"
 /**
- * hash_djb2 - convert a str
- * @str: string
- * Return: hash
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -11,6 +12,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-		hash = ((hash << 5) + hash) + c;
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	return (hash);
 }
