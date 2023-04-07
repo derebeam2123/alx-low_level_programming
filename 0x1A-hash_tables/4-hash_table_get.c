ec0aecc360b171b0c12443c6b3ba60ff462e8a35
nahom <derebeam2123@gmail.com>
2023-04-07 11:52:36 -0700

    task all

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,20 +1,25 @@
 #include "hash_tables.h"
 /**
- * hash_table_get - to get hash
- * @ht: table hash
- * @key: key
- * Return: NULL
- */
+* hash_table_get - function
+* @ht: table to search
+* @key: key to retrieve
+*
+* Description: function to get value from a passed key.
+* Return: Value of key or NULL if fail
+*/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *tempNode;
 	unsigned long int index;
+
 	if (ht == NULL)
 		return (NULL);
-	if (key == NULL  || (strcmp(key, "") == 0))
+
+	if (key == NULL || (strcmp(key, "") == 0))
 		return (NULL);
 	index = (hash_djb2((const unsigned char *)key) % (ht->size));
 	tempNode = ht->array[index];
+
 	while (tempNode)
 	{
 		if (strcmp(key, tempNode->key) == 0)
