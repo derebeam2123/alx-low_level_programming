dfa7d36820e1f79b89723772dcdbebf9c8a7747c
nahom <derebeam2123@gmail.com>
2023-07-06 09:43:56 -0700

    task al

--- 0x06-pointers_arrays_strings/100-main.c
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13.\n";
 	char *p;
 
 	 p = rot13(s);
@@ -26,4 +26,3 @@ int main(void)
 	 printf("%s", s);
 	 return (0);
 }
-
