89bbbdaff68ef5547bc3974053d57fd6017b0b18
nahom <derebeam2123@gmail.com>
2023-07-16 11:50:19 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,45 +1,30 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 /**
- * create_file - function with two arguments
- * @filename: pointer to string name
- * @text_content: pointer to string contents
- *
- * Description: creates a file
- * Return: 1 on success or -1 failure
+ * create_file - create a file with read/write access for user
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int count = 0;
-	int fd = 0;
-	int output = 0;
+	int fd, rstatus, i;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		while (text_content[count] != '\0')
-			count++;
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		rstatus = write(fd, text_content, i);
+		if (rstatus == -1)
+			return (-1);
 	}
-	else
-	{
-		close(fd);
-		return (1);
-	}
-
-	output = write(fd, text_content, count);
-	if (output == -1 || output != count)
-		return (-1);
 
 	close(fd);
 	return (1);
