e82cc7be8365b6a3a1eaf57bdbddf5c242cba886
Nahom <derebeam2123@gmail.com>
2023-07-29 08:32:20 -0700

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-gcc -wall -wextra -werror -pedantic -c -fPIC -c *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
+// A script that creates a dynamic library called liball.so
+
+gcc *.c -fPIC -shared -o liball.so
